4b66e2ec218f585e0a549a27a9312e11505d3aae
Tzvi-H <16guitar@gmail.com>
2020-01-10 11:49:19 +0800

    completed first 2 object_creation_patterns exercises

---
+++ object_creation_patterns.js/ancestors.js
@@ -0,0 +1,25 @@
+// name property added to make objects easier to identify
+let foo = {name: 'foo'};
+let bar = Object.create(foo);
+bar.name = 'bar';
+let baz = Object.create(bar);
+baz.name = 'baz';
+let qux = Object.create(baz);
+qux.name = 'qux';
+
+foo.ancestors = function() {
+  let chain = [];
+  let parent = Object.getPrototypeOf(this);
+
+  while (parent !== Object.prototype) {
+    chain.push(parent.name);
+    parent = Object.getPrototypeOf(parent);
+  }
+
+  return chain.concat('Object.prototype');
+}
+
+console.log(qux.ancestors());  // returns ['baz', 'bar', 'foo', 'Object.prototype']
+console.log(baz.ancestors());  // returns ['bar', 'foo', 'Object.prototype']
+console.log(bar.ancestors());  // returns ['foo', 'Object.prototype']
+console.log(foo.ancestors());  // returns ['Object.prototype']
