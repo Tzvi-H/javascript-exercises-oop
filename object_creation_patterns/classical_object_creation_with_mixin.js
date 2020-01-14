59479f7cce94be6977353bb964330406593494ff
Tzvi-H <16guitar@gmail.com>
2020-01-14 11:33:29 +0800

    completed first 4 exercises

---
+++ object_creation_patterns/classical_object_creation_with_mixin.js
@@ -0,0 +1,115 @@
+let professional = {
+  invoice() {
+    console.log(`${this.fullName()} is Billing customer`);
+  },
+
+  payTax() {
+    console.log(`${this.fullName()} Paying taxes`);
+  }
+} 
+
+function Person(firstName, lastName, age, gender) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.gender = gender;
+}
+
+Person.prototype.fullName = function() {
+  return this.firstName + ' ' + this.lastName;
+}
+
+Person.prototype.communicate = function() {
+  console.log('Communicating');
+}
+
+Person.prototype.eat = function() {
+  console.log('Eating');
+}
+
+Person.prototype.sleep = function() {
+  console.log('Sleeping');
+}
+
+function Doctor(firstName, lastName, age, gender, specialization) {
+  Person.apply(this, arguments);
+  this.specialization = specialization;
+}
+Doctor.prototype = Object.create(Person.prototype);
+Doctor.prototype.constructor = Doctor;
+
+Doctor.prototype.diagnose = function() {
+  console.log('Diagnosing');
+}
+
+
+function Professor(firstName, lastName, age, gender, subject) {
+  Person.apply(this, arguments);
+  this.subject = subject;
+}
+Professor.prototype = Object.create(Person.prototype);
+Professor.prototype.constructor = Professor;
+
+Professor.prototype.teach = function() {
+  console.log('Teaching');
+}
+
+function Student(firstName, lastName, age, gender, degree) {
+  Person.apply(this, arguments);
+  this.degree = degree;
+}
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.study = function() {
+  console.log('Studying');
+}
+
+function GraduateStudent(firstName, lastName, age, gender, degree, graduateDegree) {
+  Student.apply(this, arguments);
+  this.graduateDegree = graduateDegree;
+}
+GraduateStudent.prototype = Object.create(Student.prototype);
+GraduateStudent.prototype.constructor = GraduateStudent;
+
+GraduateStudent.prototype.research = function() {
+  console.log('Researching');
+}
+
+function extend(object, mixin) {
+  for (let methodName in mixin) {
+    object.constructor.prototype[methodName] = function() {
+      return mixin[methodName].call(this);
+    }
+  }
+  return object;
+}
+
+let doctor = extend(new Doctor('foo', 'bar', 21, 'gender', 'Pediatrics'), professional);
+console.log(doctor instanceof Person);     // logs true
+console.log(doctor instanceof Doctor);     // logs true
+doctor.eat();                              // logs 'Eating'
+doctor.communicate();                      // logs 'Communicating'
+doctor.sleep();                            // logs 'Sleeping'
+console.log(doctor.fullName());            // logs 'foo bar'
+doctor.diagnose();                         // logs 'Diagnosing'
+
+let professor = extend(new Professor('foo', 'bar', 21, 'gender', 'Systems Engineering'), professional);
+console.log(professor instanceof Person);     // logs true
+console.log(professor instanceof Professor);  // logs true
+professor.eat();                              // logs 'Eating'
+professor.communicate();                      // logs 'Communicating'
+professor.sleep();                            // logs 'Sleeping'
+console.log(professor.fullName());            // logs 'foo bar'
+professor.teach();                            // logs 'Teaching'
+
+doctor.invoice();                          // logs 'foo bar is Billing customer'
+doctor.payTax();                           // logs 'foo bar Paying taxes'
+
+professional.invoice = function() {
+  console.log(this.fullName() + ' is Asking customer to pay');
+};
+
+doctor.invoice();                          // logs 'foo bar is Asking customer to pay'
+professor.invoice();                       // logs 'foo bar is Asking customer to pay'
+professor.payTax();                        // logs 'foo bar Paying taxes'
